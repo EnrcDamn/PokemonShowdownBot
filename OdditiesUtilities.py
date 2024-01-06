d7b12be8d953905fc4a70d9b1abc878b5d13349a
EnrcDamn <enrico.damiani10@gmail.com>
2024-01-06 19:07:44 +0100

    Added an oddities move list.

--- OdditiesUtilities.py
+++ OdditiesUtilities.py
@@ -1,2 +1,16 @@
 ﻿# TODO: refactor with this new file for oddities (peculiar pokemon and moves, ...)
 
+ODD_MOVES = [
+    "fakeout",
+    "explosion",
+    "selfdestruct",
+    "solarbeam",
+    "sunnyday",
+    "raindance",
+    "suckerpunch",
+    "substitute",
+    "focuspunch",
+    "transform",
+    "pursuit",
+    "aromatherapy",
+]
